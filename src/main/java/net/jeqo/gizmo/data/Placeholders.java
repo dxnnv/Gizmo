3e5f18c93840781402eb4bc63a75964ee540800f
danny <danny@atropos.cc>
2025-04-26 23:59:07 -0400

    feat!: utilize minimessage, update utils

--- src/main/java/net/jeqo/gizmo/data/Placeholders.java
+++ src/main/java/net/jeqo/gizmo/data/Placeholders.java
@@ -1,29 +1,22 @@
 package net.jeqo.gizmo.data;
 
 import net.jeqo.gizmo.Gizmo;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
 
-import static net.jeqo.gizmo.data.Utilities.pullConfig;
-import static net.jeqo.gizmo.data.Utilities.pullScreensConfig;
+import static net.jeqo.gizmo.data.Utilities.*;
 
 public class Placeholders {
+    static final Gizmo plugin = Gizmo.getInstance();
 
-    static Gizmo plugin = Gizmo.getPlugin(Gizmo.class);
+    private static final String DEFAULT_PREFIX = "<#ee0000>[Gizmo] ";
 
+    public static String getPrefix() {
+        if (plugin == null) return DEFAULT_PREFIX;
 
-
-    public static String gizmoPrefix() {
-        return "#ee0000[Gizmo] ";
+        String prefix = plugin.getConfig().getString("prefix", DEFAULT_PREFIX);
+        return prefix.isBlank() ? DEFAULT_PREFIX : prefix;
     }
-    public static String gizmoPrefixNoDec() {
-        return "#ee0000Gizmo";
-    }
 
-
-
-    public static String shift48() {
-        return plugin.getScreensConfig().getString("Unicodes.shift-48");
-    }
     public static String shift1013() {
         return plugin.getScreensConfig().getString("Unicodes.shift-1013");
     }
@@ -31,12 +24,10 @@ public class Placeholders {
         return plugin.getScreensConfig().getString("Unicodes.shift-1536");
     }
 
-
-
-    public static String screenTitle() {
-        return pullConfig("background-color") + shift1013() + pullScreensConfig("Unicodes.background") + shift1536() + ChatColor.WHITE + pullScreensConfig("Unicodes.welcome-screen");
+    public static Component screenTitle() {
+        return createComponent(plugin.getConfig().getString("background-color") + shift1013() + pullScreensConfig("Unicodes.background") + shift1536() + "<white>" + pullScreensConfig("Unicodes.welcome-screen"));
     }
-    public static String screenTitleFirstJoin() {
-        return pullConfig("background-color") + shift1013() + pullScreensConfig("Unicodes.first-join-background") + shift1536() + ChatColor.WHITE + pullScreensConfig("Unicodes.first-join-welcome-screen");
+    public static Component screenTitleFirstJoin() {
+        return createComponent(plugin.getConfig().getString("background-color") + shift1013() + pullScreensConfig("Unicodes.first-join-background") + shift1536() + "<white>" + pullScreensConfig("Unicodes.first-join-welcome-screen"));
     }
 }
--- src/main/java/net/jeqo/gizmo/data/Utilities.java
+++ src/main/java/net/jeqo/gizmo/data/Utilities.java
@@ -2,56 +2,58 @@ package net.jeqo.gizmo.data;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.jeqo.gizmo.Gizmo;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import java.time.Duration;
 
 public class Utilities {
-    static Gizmo plugin = Gizmo.getPlugin(Gizmo.class);
+    static final Gizmo plugin = Gizmo.getInstance();
 
-    public static String chatTranslate(String message) {
-        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-        Matcher matcher = pattern.matcher(message);
-        while (matcher.find()) {
-            String hexCode = message.substring(matcher.start(), matcher.end());
-            String replaceSharp = hexCode.replace('#', 'x');
+    public static Component createComponent(String string, TagResolver... resolvers) {
+        if (string == null) return Component.empty();
+        return MiniMessage.miniMessage().deserialize(string, resolvers);
+    }
 
-            char[] ch = replaceSharp.toCharArray();
-            StringBuilder builder = new StringBuilder("");
-            for (char c : ch) {
-                builder.append("&" + c);
-            }
-
-            message = message.replace(hexCode, builder.toString());
-            matcher = pattern.matcher(message);
+    public static Component createComponent(Player player, String string, TagResolver... resolvers) {
+        if (string == null) return Component.empty();
+        if (player != null) {
+            string = PlaceholderAPI.setPlaceholders(player, string);
         }
-        return PlaceholderAPI.setPlaceholders(null, ChatColor.translateAlternateColorCodes('&', message));
+        return createComponent(string, resolvers);
     }
 
+    public static Component screensComponent(String id) {
+        return pullScreensConfig(id) instanceof String ? createComponent((String) pullScreensConfig(id)) : Component.empty();
 
-
-    public static void log(@NotNull String text) {
-        Bukkit.getLogger().log(Level.INFO, "[Gizmo] " + text);
-    }
-    public static void warn(@NotNull String text) {
-        Bukkit.getLogger().log(Level.WARNING, "[Gizmo] " + text);
     }
 
-
-
-    public static String pullConfig(String id) {
-        return Utilities.chatTranslate(plugin.getConfig().getString(id, ""));
+    public static Object pullConfig(String id) {
+        return plugin.getConfig().contains(id) ? plugin.getConfig().get(id) : null;
     }
-    public static String pullScreensConfig(String id) {
-        return Utilities.chatTranslate(plugin.getScreensConfig().getString(id, ""));
+
+    public static Object pullScreensConfig(String id) {
+        return plugin.getScreensConfig().contains(id) ? plugin.getScreensConfig().get(id) : null;
     }
-    public static String pullMessagesConfig(String id) {
-        return Utilities.chatTranslate(plugin.getMessagesConfig().getString(id, ""));
+
+    public static Object pullMessagesConfig(String id) {
+        return plugin.getMessagesConfig().contains(id) ? plugin.getMessagesConfig().get(id) : null;
+    }
+
+    public static void showTitle(final Audience target, @Nullable Component mainTitle, @Nullable Component subtitle, int fadeIn, int stay, int fadeOut) {
+        if (!(target instanceof Player)) return;
+
+        if (mainTitle == null) mainTitle = Component.empty();
+        if (subtitle == null) subtitle = Component.empty();
+
+        final Title.Times times = Title.Times.times(Duration.ofMillis(fadeIn), Duration.ofMillis(stay), Duration.ofMillis(fadeOut));
+        final Title title = Title.title(mainTitle, subtitle, times);
+
+        target.showTitle(title);
     }
 }
