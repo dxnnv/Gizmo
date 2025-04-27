8bbebf7eea0082bfc7b2e561e792b8718623c542
danny <danny@atropos.cc>
2025-04-27 00:03:49 -0400

    refactor!: main class refactor, remove metrics + update checker

--- src/main/java/net/jeqo/gizmo/Gizmo.java
+++ src/main/java/net/jeqo/gizmo/Gizmo.java
@@ -1,13 +1,12 @@
 package net.jeqo.gizmo;
 
+import lombok.Getter;
 import net.jeqo.gizmo.data.*;
 import net.jeqo.gizmo.listeners.*;
 import org.bukkit.Bukkit;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -16,35 +15,23 @@ import java.util.HashMap;
 import java.util.Objects;
 import java.util.UUID;
 
-public final class Gizmo extends JavaPlugin implements Listener {
+public final class Gizmo extends JavaPlugin {
+
+    public static final HashMap<UUID, String> playerTracker = new HashMap<>();
+    @Getter
+    public static Gizmo instance;
 
     @Override
     public void onEnable() {
-        Utilities.log("|---[ GIZMO ]--------------------------------------------------------|");
-        Utilities.log("|                           Plugin loaded.                           |");
-        Utilities.log("|-------------------------------------------------[ MADE BY JEQO ]---|");
-
         instance = this;
-        loadListeners(); loadCommands();
-        Metrics metrics = new Metrics(this, pluginId); updateChecker();
+        loadListeners();
+        Objects.requireNonNull(getCommand("gizmo")).setExecutor(new Commands(this));
 
-        getConfig().options().copyDefaults(); saveDefaultConfig();
-        createScreensConfig(); createMessagesConfig();
-    }
-    @Override
-    public void onDisable() {
-        Utilities.log("|---[ GIZMO ]--------------------------------------------------------|");
-        Utilities.log("|                          Shutting down...                          |");
-        Utilities.log("|-------------------------------------------------[ MADE BY JEQO ]---|");
-    }
-
-
-
-    public static HashMap<UUID, String> playerTracker = new HashMap<>();
-    public static Gizmo instance;
-    int pluginId = 16873;
-    public static Gizmo getInstance() {
-        return instance;
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        createScreensConfig();
+        createMessagesConfig();
+        getLogger().info("Gizmo successfully loaded! Made by Jeqo");
     }
 
     public void loadListeners() {
@@ -53,67 +40,43 @@ public final class Gizmo extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(new ScreenAdvance(), this);
         Bukkit.getPluginManager().registerEvents(new ClickableItems(), this);
     }
-    public void loadCommands() {
-        Objects.requireNonNull(getCommand("gizmo")).setExecutor(new Commands());
-        TabCompleter tc = new CommandsTabManager(); Objects.requireNonNull(this.getCommand("gizmo")).setTabCompleter(tc);
-    }
 
-
-
-    public void updateChecker() {
-        new UpdateChecker(this, 106024).getVersion(version -> {
-            if (!this.getDescription().getVersion().equals(version)) {
-                Utilities.warn("|---[ GIZMO ]--------------------------------------------------------|");
-                Utilities.warn("|                  There is a new update available!                  |");
-                Utilities.warn("|                       https://jeqo.net/gizmo                       |");
-                Utilities.warn("|-------------------------------------------------[ MADE BY JEQO ]---|");
-            }
-        });
-    }
-
-
-
-    // screens.yml
-    private File screensConfigFile;
+    @Getter
     private FileConfiguration screensConfig;
-    public FileConfiguration getScreensConfig() {
-        return this.screensConfig;
-    }
     private void createScreensConfig() {
-        screensConfigFile = new File(getDataFolder(), "screens.yml");
+        // screens.yml
+        File screensConfigFile = new File(getDataFolder(), "screens.yml");
         if (!screensConfigFile.exists()) {
-            screensConfigFile.getParentFile().mkdirs();
-            saveResource("screens.yml", false);
+            if (screensConfigFile.getParentFile().mkdirs()) {
+                saveResource("screens.yml", false);
+            }
         }
 
         screensConfig = new YamlConfiguration();
         try {
             screensConfig.load(screensConfigFile);
         } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
+            getLogger().warning("Error while loading screens config! " + e.getMessage());
         }
     }
 
 
-
-    // messages.yml
-    private File messagesConfigFile;
+    @Getter
     private FileConfiguration messagesConfig;
-    public FileConfiguration getMessagesConfig() {
-        return this.messagesConfig;
-    }
     private void createMessagesConfig() {
-        messagesConfigFile = new File(getDataFolder(), "messages.yml");
+        // messages.yml
+        File messagesConfigFile = new File(getDataFolder(), "messages.yml");
         if (!messagesConfigFile.exists()) {
-            messagesConfigFile.getParentFile().mkdirs();
-            saveResource("messages.yml", false);
+            if(messagesConfigFile.getParentFile().mkdirs()) {
+                saveResource("messages.yml", false);
+            }
         }
 
         messagesConfig = new YamlConfiguration();
         try {
             messagesConfig.load(messagesConfigFile);
         } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
+            getLogger().warning("Error while loading messages! " + e.getMessage());
         }
     }
 }
